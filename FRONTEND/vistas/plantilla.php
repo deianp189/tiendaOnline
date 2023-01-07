5744a2e2878434ef7d52a26371803837cd5da6d4
deianp189 <dp189@inlumine.ual.es>
2023-01-07 14:11:50 +0100

    Creando cabecera

--- FRONTEND/vistas/plantilla.php
+++ FRONTEND/vistas/plantilla.php
@@ -3,10 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="title" content="Casari">
+    <meta name="description" content="Casari es una tienda en línea de materiales de construcción ubicada en Granada">
+    <meta name="keyword" content="construccion, azulejos, cemento, tejas, bañera, cristales">
     <title>Casari</title>
+
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed">
+
+    <script src="vistas/js/plugins/jquery.min.js"></script>
+    <script src="vistas/js/plugins/bootstrap.min.js"></script>
 </head>
 <body>
-    <h1>Hola mundo</h1>
+    <!--------------------------- Cabecera ------------------------------>
+
+    <?php 
+    include "modulos/cabecera.php";
+    ?>
+
 </body>
 </html>
